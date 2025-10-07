b123d53b588d7166b73cbcff2d06779bc0661ce4
viggidbest <viggi.26@gmail.com>
2025-10-07 18:33:30 +0530

    New 1

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,10 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+
 export default defineConfig({
   plugins: [vue()],
-  server: { proxy: { '/api': 'http://localhost:8080' } }
+  server: { proxy: { '/api': 'http://localhost:8080' } },
+  test: {
+    environment: 'jsdom',  // <- This makes Vitest run with a browser-like DOM
+  },
 })
