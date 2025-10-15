fa46d8d765e737aea8593897d298b5be0eb9b41a
viggidbest <viggi.26@gmail.com>
2025-10-15 16:30:42 +0530

    Front end

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -3,11 +3,12 @@ import { describe, it, expect } from 'vitest'
 describe('Dummy test suite', () => {
   it('always passes', () => {
     expect(true).toBe(true)
+    
   })
 })
 
-describe('Intentional failure test', () => {
-  it('should fail on purpose', () => {
-    expect(true).toBe(false) // ❌ This will always fail
-  })
-})
+// describe('Intentional failure test', () => {
+//   it('should fail on purpose', () => {
+//     expect(true).toBe(false) // ❌ This will always fail
+//   })
+// })
