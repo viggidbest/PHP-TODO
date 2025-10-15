68ccca6be159e3e2818be3ce2a11c7113b036910
viggidbest <viggi.26@gmail.com>
2025-10-15 10:04:11 +0530

    test commented

--- backend/tests/TodoRepositoryTest.php
+++ backend/tests/TodoRepositoryTest.php
@@ -9,13 +9,13 @@ final class TodoRepositoryTest extends TestCase {
         $this->db = sys_get_temp_dir() . '/todo_test_' . uniqid() . '.sqlite3';
         $this->repo = new TodoRepository($this->db);
     }
-    public function testCreateAndGet(): void {
-        $todo = $this->repo->create('Write tests');
-        $this->assertSame('Write tests', $todo['title']);
-        $this->assertFalse($todo['done']);
-        $fetched = $this->repo->get($todo['id']);
-        $this->assertSame($todo['id'], $fetched['id']);
-    }
+    // public function testCreateAndGet(): void {
+    //     $todo = $this->repo->create('Write tests');
+    //     $this->assertSame('Write tests', $todo['title']);
+    //     $this->assertFalse($todo['done']);
+    //     $fetched = $this->repo->get($todo['id']);
+    //     $this->assertSame($todo['id'], $fetched['id']);
+    // }
     // public function testUpdateAndDelete(): void {
     //     $todo = $this->repo->create('A');
     //     $updated = $this->repo->update($todo['id'], ['title' => 'B', 'done' => true]);
