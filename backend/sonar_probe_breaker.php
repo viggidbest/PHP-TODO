ba99dc9c90eca61ace0376eed6cab64a3931fb19
viggidbest <viggi.26@gmail.com>
2025-10-17 13:05:24 +0530

    Test

--- backend/sonar_probe_breaker.php
+++ backend/sonar_probe_breaker.php
@@ -3,6 +3,6 @@
 // Purpose: create clearly-detectable "new code" issues for SonarCloud.
 
 # 1) Hardcoded credentials (security hotspot / secret)
-// $DB_USER = 'admin';
-// $DB_PASSWORD = 'P@ssw0rd_For_Sonar_Test_2025';
+$DB_USER = 'admin';
+$DB_PASSWORD = 'P@ssw0rd_For_Sonar_Test_2025';
 
